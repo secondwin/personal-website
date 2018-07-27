bf6cbb44015b3ca94eca87eb778b67c611e5d230
Thomas Nguyen <tnguy162@calpoly.edu>
2018-07-26 17:05:39 -0700

    Add document title

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount(){
+    document.title = "Thomas Nguyen"
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Thomas Nguyen</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          In development...
         </p>
       </div>
     );
